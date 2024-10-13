5985c37dd38f2d6c89ced2cb91c0357fb496bfed
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-10-13 18:35:37 +0530

    Refactor code for Check Prime (Optimized) class

---
+++ 15 Functions And Methods/Check Prime/Prime.java
@@ -0,0 +1,19 @@
+public class Prime {
+    public static boolean checkPrime(int a)
+    {
+        if(a==2)
+            return true;
+        
+        for (int i=2;i<=a-1;i++)
+        {
+            if(a%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPrime(5));
+        System.out.println(checkPrime(20));
+    }
+}
