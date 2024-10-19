2dc15e2f080b769a9fcc75183ec405488f402382
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-10-19 21:53:37 +0530

    Refactor code for PrintSubArraySum class to include counting the total number of subarrays

---
+++ 17 Arrays (Part-1)/Print Sub Array Sum/PrintSubArraySum.java
@@ -0,0 +1,27 @@
+public class PrintSubArraySum 
+{
+    static void printSubarray(int[] arr)
+    {
+        int ts=0;
+        for(int i=0; i<arr.length;i++)                      // To iterate over the element of the array 
+        {
+            for (int j=i;j<arr.length;j++)                  // To iterate over the array from i to last to print the sub array.
+            {   
+                int subArraySum=arr[i];
+                for (int k=i;k<j;k++)                       // To actually print the subarrays sum.
+                {
+                    subArraySum+=arr[k+1];
+                }
+                ts++;
+                System.out.print(subArraySum+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of subarrays is:"+ts);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8};
+
+        printSubarray(arr);
+    }
+}
