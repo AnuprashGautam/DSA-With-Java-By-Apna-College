1925ec4adfff387dd9c3125aaa4661f2b4746c71
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-28 00:08:21 +0530

    Rename formPair method to findMaxSubArraySum and implement prefix array for maximum subarray sum calculation

--- 18 Arrays (Part-2)/Max Sub Array Sum (Prefix Array)/MaxSubArraySum.java
+++ 18 Arrays (Part-2)/Max Sub Array Sum (Prefix Array)/MaxSubArraySum.java
@@ -4,23 +4,29 @@ public class MaxSubArraySum
     static void findMaxSubArraySum(int[] array)
     {
         int maxSubArraySum = Integer.MIN_VALUE;
+        int subArraySum=0;
+
+        // Constructing the prefix array.
+        int[] prefixArray= new int[array.length];
+        prefixArray[0]=array[0];
+
+        for (int i = 1; i < prefixArray.length; i++) {
+            prefixArray[i]=prefixArray[i-1]+array[i];
+        }
 
         for (int i = 0; i < array.length; i++) 
         {
+            int start = i;
             for (int j = i; j < array.length; j++) {
-                int subArraySum=array[i];
-                for (int k = i+1; k <= j; k++) {
-                    subArraySum +=array[k];
-                }
+                int end = j;
+                
+                subArraySum = (start==0) ? prefixArray[end] : prefixArray[end]-prefixArray[start-1];
 
                 if(subArraySum >= maxSubArraySum)
                     maxSubArraySum=subArraySum;
 
-                System.out.print(subArraySum+"  ");
             }
-            System.out.println();
         }
-        System.out.println();
         System.out.println("Maximum sub array in the whole parent array: "+ maxSubArraySum);
     }
 
