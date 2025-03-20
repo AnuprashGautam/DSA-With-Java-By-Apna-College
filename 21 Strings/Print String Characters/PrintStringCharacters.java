096b1182c88936dfda7fd751fdf7d23c872dfb4d
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-03-20 13:13:45 +0530

    Add PrintStringCharacters and StringPalindrome classes for character printing and palindrome checking

---
+++ 21 Strings/Print String Characters/PrintStringCharacters.java
@@ -0,0 +1,9 @@
+public class PrintStringCharacters {
+    public static void main(String[] args) {
+        String str="Anuprash Gautam";
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i)+" ");
+        }
+    }
+}
---
+++ 21 Strings/String Palindrome/StringPalindrome.java
@@ -0,0 +1,16 @@
+class StringPalindrome {
+    static boolean isStringPalindrome(String str)
+    {
+        for (int i = 0; i < str.length()/2; i++) {
+            if(str.charAt(i)!=str.charAt(str.length()-1-i))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str="madam";
+
+        System.out.println("Is 'madam' palidrome string: "+isStringPalindrome(str));
+    }
+}
