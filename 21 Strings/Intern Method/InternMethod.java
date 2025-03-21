9a8e42ed62ca4ff2ab33004acb1555bda33739ed
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-03-21 15:10:01 +0530

    Add InternMethod class to demonstrate String interning behavior

---
+++ 21 Strings/Intern Method/InternMethod.java
@@ -0,0 +1,14 @@
+class InternMethod {
+    public static void main(String[] args) {
+        String str1="hello";
+        String str2=new String("hello");
+        String str3=new String("hello").intern();
+        String str4=str2.intern();
+
+
+        System.out.println(str1==str2);
+        System.out.println(str1==str3);
+        System.out.println(str2==str3);
+        System.out.println(str4==str1);
+    }
+}
