3200415e384d6bc50c359a3c5f8f51c3f60594ec
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-04-01 17:31:59 +0530

    Add FirstOccurence class to find the first occurrence of a target in an array using recursion

---
+++ 24 Recursion Baiscs (Part 1)/First Occurence/FirstOccurence.java
@@ -0,0 +1,22 @@
+public class FirstOccurence
+{
+    static int firstOccurence(int[] arr, int target, int start)
+    {
+        if(start==arr.length)
+        {
+            return -1;
+        }
+        if(target==arr[start])
+        {
+            return start;
+        }
+        return firstOccurence(arr, target, start+1);
+    }
+
+    public static void main (String[] args)
+    {
+        int[] arr={1,2,3,4,6,7,8,5};
+
+        System.out.println("Is there exists 5 in this array? "+firstOccurence(arr,5,0));
+    }
+}
